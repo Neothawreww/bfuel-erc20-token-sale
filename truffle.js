a5eb7816dc98e2df1d0d87ee8739dda5696d971b
Neo Thawreww <neothawreww@protonmail.com>
2022-04-16 01:28:40 -0500

    added ropsten and infura

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = "prize jaguar monkey shadow rose inquiry matter liberty horror cargo brief remove cave task era";
+
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -19,12 +25,18 @@ module.exports = {
             from: 'cdbeba354b8ade195a9467c9b56da19bffaf7dc9'
         },
         testNet: {
-            host: "localhost",
+            host: "https://rinkeby.infura.io/v3/9fa7a488e03f4b4aae7f256dc949708d",
             port: 8566,
-            network_id: "*", // Match any network id
+            network_id: "4", // Match any network id
             gasPrice: 1e11,
             gas: 4700000,
             from: '0x1f82285da1d89992d4dadf02dbc89f18a5ffc6ed'
+        },
+        ropsten: {
+          provider: function() {
+           return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/9fa7a488e03f4b4aae7f256dc949708d")
+          },
+         network_id: 3
         },
         mainNet: {
             network_id: 1,
